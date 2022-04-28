cb4299bf6ada9aaeb2a7d25206473ef702d55534
DerekAlmanza <derek.almanza@ciencias.unam.mx>
2022-04-27 21:31:03 -0500

    Agregados Navbar and toggle menu.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
-const menuToggle = document.querySelector(".menuToggle");
+// Navbar and toggle menu.
+const toggleMenu = document.getElementById("toggleMenu");
+const navigation = document.getElementById("navigation");
 
-menuToggle.onclick = function () {
-  menuToggle.classList.toggle("active");
-};
+const header = document.querySelector("header");
+
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("active");
+  navigation.classList.toggle("active");
+});
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
